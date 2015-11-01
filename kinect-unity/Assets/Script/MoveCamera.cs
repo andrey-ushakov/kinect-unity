1c3ab4bf4e74ebc6c91f112c44e8aeed7f58134a
AndreyU <andrey200964@yandex.ru>
2015-11-01 19:09:44 +0100

    Updated Headtracking scene

---
+++ kinect-unity/Assets/Script/MoveCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MoveCamera : MonoBehaviour {
+    public float speed = 10.0F;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+        float translX = Input.GetAxis("Horizontal") * speed;
+        translX *= Time.deltaTime;
+
+        float translY = Input.GetAxis("Vertical") * speed;
+        translY *= Time.deltaTime;
+
+        float translZ = Input.GetAxis("Zoom") * speed;
+        translZ *= Time.deltaTime;
+
+        transform.Translate(translX, translY, translZ);
+    }
+}
