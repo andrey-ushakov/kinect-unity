bf12c70d1fa1f19fb91ce731ac7a7778a421e3bb
AndreyU <andrey200964@yandex.ru>
2015-11-01 18:03:32 +0100

    Action 1 : Updated scene.

---
+++ kinect-unity/Assets/Script/wallTexture.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallTexture : MonoBehaviour {
+
+	void Start () {
+        transform.GetComponent<Renderer>().material.mainTextureScale = new Vector2(1, 100);
+    }
+	
+	void Update () {
+
+    }
+}
