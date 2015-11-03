cf08440d2a27961baad252f067d984d41f621388
pangboww <pangboww@gmail.com>
2015-11-03 11:38:25 +0100

    add handInputManager

--- kinect-unity/Assets/Script/MoveCamera.cs
+++ kinect-unity/Assets/Script/MoveCamera.cs
@@ -11,15 +11,31 @@ public class MoveCamera : MonoBehaviour {
 	
 	void Update () {
 
-        float translX = Input.GetAxis("Horizontal") * speed;
-        translX *= Time.deltaTime;
+        //float translX = Input.GetAxis("Horizontal") * speed;
+        //translX *= Time.deltaTime;
 
-        float translY = Input.GetAxis("Vertical") * speed;
-        translY *= Time.deltaTime;
+        //float translY = Input.GetAxis("Vertical") * speed;
+        //translY *= Time.deltaTime;
 
-        float translZ = Input.GetAxis("Zoom") * speed;
-        translZ *= Time.deltaTime;
+        //float translZ = Input.GetAxis("Zoom") * speed;
+        //translZ *= Time.deltaTime;
 
-        transform.Translate(translX, translY, translZ);
+        //transform.Translate(translX, translY, translZ);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Camera.main.transform.Translate(0f, speed, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Camera.main.transform.Translate(0f, -speed, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Camera.main.transform.Translate(-speed, 0f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Camera.main.transform.Translate(speed, 0f, 0f);
+        }
+
     }
 }
