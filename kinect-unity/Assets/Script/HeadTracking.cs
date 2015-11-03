4f8d30d7a5968fbc8b16276652ba848f8e0d99a1
pangboww <pangboww@gmail.com>
2015-11-04 00:30:33 +0100

    update head tracking

--- kinect-unity/Assets/Script/HeadTracking.cs
+++ kinect-unity/Assets/Script/HeadTracking.cs
@@ -6,14 +6,10 @@ public class HeadTracking : MonoBehaviour {
 	
 	private SkeletonWrapper sw;
     private HandInputManager him;
-	public GameObject Head;
-	
-	public int player;
-	
-	public float scale = 1.0f;
-    /* Début ajout F. Davesne */
-    public bool isTracked;
-	/* Fin ajout F. Davesne */
+    private int PlayerId = 0;
+
+    private Vector3 headPos;
+
     void Awake() {
         sw = SkeletonWrapper.Instance;
         him = HandInputManager.Instance;
@@ -21,29 +17,11 @@ public class HeadTracking : MonoBehaviour {
 
 
 	void Start () {
-        isTracked = false;
+
 	}
 	
 	void Update () {
-        //if (player == -1) {
-        //    isTracked = false;
-        //    return;
-        //}
-
-        //if (sw.pollSkeleton()) {
-        //    Debug.Log("After pollSkeleton() ");
-        //    isTracked = false;
-
-        //    transform.localPosition = new Vector3(
-        //            sw.bonePos[player, (int)Kinect.NuiSkeletonPositionIndex.Head].x * scale,
-        //            -sw.bonePos[player, (int)Kinect.NuiSkeletonPositionIndex.Head].y * scale,
-        //            sw.bonePos[player, (int)Kinect.NuiSkeletonPositionIndex.Head].z * scale - 11);
-
-        //    Debug.Log(sw.bonePos[player, (int)Kinect.NuiSkeletonPositionIndex.Head].x * scale);
-
-        //    /* Ajout F. Davesne pour repérer si les éléments du squelette intéressants sont trackés => isTracked */
-        //    isTracked = isTracked | (sw.boneState[player, (int)Kinect.NuiSkeletonPositionIndex.Head] == Kinect.NuiSkeletonPositionTrackingState.Tracked);
-        //    /* Fin Ajout F. Davesne */
-        //}
+        this.headPos = sw.bonePos[PlayerId, 3];
+        Camera.main.transform.position = this.headPos + new Vector3(2, 1, -8);
     }
 }
