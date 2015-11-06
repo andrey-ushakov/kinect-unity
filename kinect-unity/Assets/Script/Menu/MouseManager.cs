ea1ee577f5b43fdcae9ad7b18fd8186b117959cc
pangboww <pangboww@gmail.com>
2015-11-06 22:22:09 +0100

    add MouseManger

---
+++ kinect-unity/Assets/Script/Menu/MouseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+	Vector2 mouse;
+	int w = 64;
+	int h = 64;
+	public Texture2D cursor;
+	
+	void Start()
+	{
+		Cursor.visible = false;
+	}
+	
+	void Update()
+	{
+		mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+	}
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(mouse.x - (w / 2), mouse.y - (h / 2), w, h), cursor);
+	}
+
+}
