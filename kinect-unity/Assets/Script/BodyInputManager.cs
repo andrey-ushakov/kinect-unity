d07c44cd37382e8263a77a3d3829d2e4c0be0c5a
pangboww <pangboww@gmail.com>
2015-11-09 18:16:36 +0100

    add BodyInputManager

---
+++ kinect-unity/Assets/Script/BodyInputManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public enum BodyMotion { 
+	NON,
+	JUMP,
+	SUPER_JUMP
+}
+
+public class BodyMotionDetectedEventArgs: EventArgs{
+	public BodyMotion motion = BodyMotion.NON;
+}
+
+public delegate void BodyMotionDetectedEventHandler(object sender, BodyMotionDetectedEventArgs arg);
+
+
+public class BodyInputManager : MonoBehaviour {
+
+	public static event BodyMotionDetectedEventHandler handMotionDetected;
+	
+	private SkeletonWrapper sw;
+	private int PlayerId = 0;
+	
+	public float detectPositionDuration = 1f;
+
+	private Vector3 rightFootPos;
+	private Vector3 leftFootPos;
+	private Vector3 spinePos;
+	private Vector3 rightFootPosPre = Vector3.zero;
+	private Vector3 leftFootPosPre = Vector3.zero;
+	private Vector3 spinePosPre = Vector3.zero;
+
+	public float detectMotionDuration = 0.4f; 
+	public float detectMotionDistance = 0.2f;
+
+	private Vector3 rightFootVelo;
+	private Vector3 leftFootVelo;
+	private Vector3 spineVelo;
+
+	public float accuracy = 0.05f;
+	
+	private static BodyInputManager _instance;
+	
+	public static BodyInputManager Instance 
+	{
+		get {
+			_instance = FindObjectOfType(typeof(BodyInputManager)) as BodyInputManager;
+			if (_instance == null) {
+				_instance = GameObject.Instantiate(GlobalVariables.GO_BODY_INPUT_MANAGER) as BodyInputManager;
+			}
+			return _instance;
+		}
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+		sw = SkeletonWrapper.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sw.pollSkeleton ()) {
+			float currentTime = Time.time;
+			this.rightFootPos = sw.bonePos[PlayerId, 19];
+			this.leftFootPos = sw.bonePos[PlayerId, 15];
+			this.spinePos = sw.bonePos[PlayerId, 1];
+		
+		}
+	}
+}
--- kinect-unity/Assets/Script/GlobalVariables.cs
+++ kinect-unity/Assets/Script/GlobalVariables.cs
@@ -10,9 +10,13 @@ public class GlobalVariables : MonoBehaviour {
     public static HandInputManager GO_HAND_INPUT_MANAGER;
     public HandInputManager handInputManager;
 
+	public static BodyInputManager GO_BODY_INPUT_MANAGER;
+	public BodyInputManager bodyInputManager;
+
     void Awake() {
         GO_KINECT_PREFAB = kinectPrefab;
         GO_HAND_INPUT_MANAGER = handInputManager;
+		GO_BODY_INPUT_MANAGER = bodyInputManager;
     }
     // Use this for initialization
     void Start()
